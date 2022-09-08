6fc1c201a21c5ee395dfa35dd3dddfbb156b015a
Lautaro Boiola <lboiola@gmail.com>
2022-09-08 10:08:44 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Expenses from './components/Expenses/Expenses';
+
+function App() {
+  const expenses = [
+    { id: 'e1', title: 'Toilet Paper', amount: 123.4, date: new Date(2020, 2, 22) },
+    { id: 'e2', title: 'Car Insurance', amount: 222.3, date: new Date(2020, 2, 25) },
+    { id: 'e3', title: 'Cats Food', amount: 333.2, date: new Date(2020, 3, 3) },
+    { id: 'e4', title: 'Basic Stuff', amount: 44133.00, date: new Date(2020, 4, 12) },
+  ]
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,34 @@
+import Card from '../UI/Card'
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+
+const ExpenseItem = (props) => {
+    
+    let expenseTitle = props.title;
+    let expenseAmount = props.amount;
+
+    const clickHandler = () => {
+        document.querySelector('#root div h2').style.cssText = 
+            `display: block;
+            border: solid red 2px;
+            margin: auto;
+            margin-top: 35px;
+            width: fit-content;
+            padding: 10px
+            ` 
+        ;
+    }
+    
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
+            </div>
+            <button style={{marginLeft: "10px", padding: "10px"}} onClick={clickHandler}>Hey, I'm a button!</button>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
